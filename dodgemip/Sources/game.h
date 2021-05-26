4e93bbb6d2565d7f13bc147782a6bef88f80d16c
Pau Torras <57144976+RostarFreeman@users.noreply.github.com>
2021-05-26 17:30:36 +0200

    Timer for frames set up

--- dodgemip/Sources/game.h
+++ dodgemip/Sources/game.h
@@ -8,10 +8,21 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+#define GAME_TOF_BIT (1 << 7)
+#define GAME_INTERRUPT_BIT (1 << 6)
+
 #define GAME_CHARACTER 0b11110100
 #define GAME_BLOCK	   0b11111111
 #define GAME_FW		   0b01111110
 #define GAME_BW		   0b01111111
 #define GAME_BLANK	   0b00100000
+
+#define GAME_CLOCKFREQ		(int)(4000000)	// 4 MHz
+#define GAME_CPUFREQ 		(int)(48000000) // 48 MHz
+#define GAME_UPDATE_RATE 	(int)(24) 		// Frames per second
+
+void setupTimer();
+void delay(int i);
+void gameLoop();
 
 #endif /* GAME_H_ */
--- dodgemip/Sources/main.c
+++ dodgemip/Sources/main.c
@@ -11,42 +11,59 @@
 
 extern inputStatus_t ibStatus;
 
+
 void delay(int i) {
 	for (; i > 0; i--) {}
 }
 
+void setupTimer() {
+	MCG_C1 = MCG_C1_IREFS_MASK | MCG_C1_IRCLKEN_MASK; 	// INTERNAL CLOCK|MCGIRCLK ACTIVE(SET)
+	MCG_C2 = MCG_C2_IRCS_MASK;                        	// SELECT FAST INTERNAL REFERENCE CLOCK (1)
+	SIM_SOPT2 |= SIM_SOPT2_TPMSRC(3);                 	// MCGIRCLK IS SELECTED FOR TPM CLOCK
+	SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK; 				  	// Enable clock to TPM0 
+	
+	TPM0_SC  = 0;             						  	// Disable timer while configuring
+	
+	// Set up for 32 fps approx
+	TPM0_SC |= TPM_SC_PS(0b010);  						// Prescaler
+	TPM0_MOD = TPM_MOD_MOD(15625); 						// Modulo value 
+	TPM0_SC |= GAME_TOF_BIT;             				// Clear TOF 
+	TPM0_SC |= GAME_INTERRUPT_BIT;       				// Enable timeout interrupt
+    TPM0_SC |= TPM_SC_CMOD(0b01);   					// Enable timer free-running mode
+    
+	NVIC_ICPR |= 0x00020000;
+	NVIC_ISER |= 0x00020000;
+}
+
+void FTM0_IRQHandler(void) 
+{
+	lcdWrite(GAME_CHARACTER);
+	TPM0_SC |= GAME_TOF_BIT;   	// Clear TOF 
+}
+
+void gameLoop() {
+	for (;;) {
+		
+	}
+}
 
 int main(void)
 {
+	// Pin setup for the input button and the LCD
 	lcdSetup();
 	ibSetup();
 	
+	// Set up LCD Screen
 	lcdFunctionSet(LCD_8BIT | LCD_LORES | LCD_2LINE);
 	lcdDisplay(LCD_CURSOROFF | LCD_DISPLAYON | LCD_BLINKOFF);
 	lcdClear();
 	lcdHome();
 	
-	for(;;) {
-		int i = 0;
-		for (i = 0; i < 32; i++) {
-			switch (ibStatus.status_word)
-			{
-			case BTN_NOBUTTON:
-				lcdWrite(GAME_BLANK);
-				break;
-			case BTN_UP_PRESSED:
-				lcdWrite(GAME_FW);
-				break;
-			case BTN_DW_PRESSED:
-				lcdWrite(GAME_BW);
-				break;
-			default:
-				lcdWrite(GAME_BLOCK);
-				break;
-			}
-			delay(25000);
-		}
-	}
+	// Set up timer
+	setupTimer();
+	
+	// Endless app loop
+	gameLoop();
 	
 	return 0;
 }
